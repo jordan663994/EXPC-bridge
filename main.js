c038bef1ac6807bd850af7646a3c7a87ff29fbec
jordan69420 <58832117+jordan69420@users.noreply.github.com>
2021-07-29 19:22:20 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const provider = window.ethereum
+
+const maticPOSClient = new MaticPOSClient({
+  network: "mainnet",
+  version: "v1",
+  parentProvider: "http://127.0.0.1:8545",
+  maticProvider: "https://rpc-mainnet.matic.network"
+});
+
+
+
+function depositEth = async () => await maticPOSClient.depositEtherForUser(from, amount, {
+  from,
+  gasPrice: "10000000000",
+});
+
+function depositERC20 = await maticPOSClient.depositERC20ForUser(rootToken, from, amount, {
+  from,
+  gasPrice: "10000000000",
+});
+
+
+function emmiter_for_ether_deposits = async () => await maticPOSClient.exitERC20(depositEth(), { from });
+
+
+function emmiter_for_ERC20_deposits = async () => await maticPOSClient.exitERC20(depositERC20(), { from });
